3eb779437632c989cef5d729650097e45732064e
Tharunikarne597 <karnetharuni31@gmail.com>
2026-02-25 14:43:49 +0530

    arthmeticoperators.java

---
+++ arthmeticoperators.java
@@ -0,0 +1,22 @@
+public class arthmeticoperators {
+
+	public static void main(String[] args) {
+		//Arithmetic operators are used to perform common mathematical operations
+		int x = 10;
+		int y = 3;
+
+		System.out.println(x + y); // 13
+		System.out.println(x - y); // 7
+		System.out.println(x * y); // 30
+		System.out.println(x / y); // 3
+		System.out.println(x % y); // 1
+
+		int z = 5;
+		++z;
+		System.out.println(z); // 6
+		--z;
+		System.out.println(z); // 5
+	
+	}
+
+}
