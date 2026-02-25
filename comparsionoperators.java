b198dd217019e0be1c7f1bf8a2a446e805473f7f
Tharunikarne597 <karnetharuni31@gmail.com>
2026-02-25 14:46:10 +0530

    Create comparsionoperators.java

---
+++ comparsionoperators.java
@@ -0,0 +1,29 @@
+public class comparsionoperators {
+
+public static void main(String[] args) {
+
+/*Comparison operators are used to compare two values (or variables).
+   This is important in programming, because it helps us to find answers and make decisions.
+   The return value of a comparison is either true or false.
+   These values are known as Boolean values.
+*/
+			int x=6;
+			int y=5;
+//			boolean result= x<y;
+//			boolean result= x>y;
+//			boolean result= x>=y;
+//			boolean result= x<=y;
+//			boolean result= x!=y;
+			boolean result= x==y;
+			System.out.println(result);
+			
+			double a=8.8;
+			double b=9.8;
+//			boolean res = a<=b;		
+			boolean res = a>=b;
+		
+			System.out.println(res);
+		}
+
+	}
+	
